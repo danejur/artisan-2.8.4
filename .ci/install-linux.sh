2bc492b7c4377331c312c63681a1cb771a3fd8d3
Dane <verydanejuras@gmail.com>
2024-07-01 22:28:32 -0400

    Update install-linux.sh

--- .ci/install-linux.sh
+++ .ci/install-linux.sh
@@ -9,6 +9,7 @@ sudo apt-get install -y -q fakeroot
 sudo apt-get install -y -q libdbus-1-3 libxkbcommon-x11-0 libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-xinerama0 libxcb-xfixes0 libxcb-cursor0
 
 sudo chmod 777 /var/lib/gems/2.7.0
+sudo chmod 777 /usr/local/bin
 gem install public_suffix -v 5.1.1
 gem install dotenv -v 2.8.1
 gem install fpm -v 1.12.0 # Linux build fails using 1.13.0
